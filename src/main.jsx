7900ab29a5a8144fbfb18493093448eb87b85da4
TutCode222 <tuddycarraway@gmail.com>
2025-09-06 16:08:38 -0500

    refactor: switch project to SCSS styling and update main.jsx to import styles.scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';  // ← must be here
 import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './styles.scss'; // your global SCSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
